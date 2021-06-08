fd602c584b40175955f2dddf402b98e856a2269d
diaploblical <egeh2x@gmail.com>
2021-06-07 23:43:27 -0400

    some more styling complete

--- js/site.js
+++ js/site.js
@@ -1,2 +1,20 @@
+M.AutoInit();
+
+const callback = function(entries) {
+  entries.forEach(entry => {
+    entry.target.classList.add("is-visible");
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".animated");
+targets.forEach(function(target) {
+  observer.observe(target);
+});
+
 var quoteGeneratorGitHub = document.querySelector("#qgGithub")
 quoteGeneratorGitHub.href = "https://github.com/diaploblical/Quote-Generator"
+
+var quoteGeneratorGitHub = document.querySelector("#dgGithub")
+quoteGeneratorGitHub.href = "https://github.com/diaploblical/Dittogram"
