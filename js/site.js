c33364977b701b24aab015079fa5301f81df8d9e
diaploblical <egeh2x@gmail.com>
2021-06-08 21:45:35 -0400

    more styling complete

--- js/site.js
+++ js/site.js
@@ -12,9 +12,3 @@ const targets = document.querySelectorAll(".animated");
 targets.forEach(function(target) {
   observer.observe(target);
 });
-
-var quoteGeneratorGitHub = document.querySelector("#qgGithub")
-quoteGeneratorGitHub.href = "https://github.com/diaploblical/Quote-Generator"
-
-var quoteGeneratorGitHub = document.querySelector("#dgGithub")
-quoteGeneratorGitHub.href = "https://github.com/diaploblical/Dittogram"
