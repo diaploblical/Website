0438ae6eef28ce1f257a61a64fe175bf842fb5b2
diaploblical <egeh2x@gmail.com>
2021-06-09 20:32:29 -0400

    animations done

--- js/site.js
+++ js/site.js
@@ -1,14 +1,23 @@
 M.AutoInit();
 
-const callback = function(entries) {
-  entries.forEach(entry => {
-    entry.target.classList.add("is-visible");
+const intersectionObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+      observer.unobserve(entry.target);
+    }
   });
-};
-
-const observer = new IntersectionObserver(callback);
-
-const targets = document.querySelectorAll(".animated");
-targets.forEach(function(target) {
-  observer.observe(target);
 });
+
+const elements = [...document.querySelectorAll(".animated")];
+
+elements.forEach((element) => intersectionObserver.observe(element));
+
+var scrollButton = document.querySelector("#scrollButton");
+var portfolioSection = document.querySelector("#portfolio");
+
+function scrollDown(target) {
+  target.scrollIntoView(true);
+}
+
+scrollButton.addEventListener("click", () => {scrollDown(portfolioSection)});
